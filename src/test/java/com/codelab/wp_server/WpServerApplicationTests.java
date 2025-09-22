91b3bc0b4b107c0e7abbebe22545ad8099dd6424
lucasmoraist <luksmnt1101@gmail.com>
2025-09-21 22:49:32 -0300

    first commit

---
+++ src/test/java/com/codelab/wp_server/WpServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codelab.wp_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WpServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
